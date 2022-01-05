c6514abeb1c9f2204487d1786365bb3e2597d67c
Christian León Pérez Serapio <cleonps@gmail.com>
2022-01-04 21:18:32 -0600

    Initial commit

---
+++ Conecta4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Conecta4
+//
+//  Created by Christian León Pérez Serapio on 04/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
